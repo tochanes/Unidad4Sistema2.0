8a4c19c5ce3af70dd02f9f1f1e00c5f5e2772182
JanPol2000 <46852049+JanPol2000@users.noreply.github.com>
2019-05-11 11:51:20 -0500

    RuntimeException

--- POOGPOAU4EQUIPO2/src/SRC/FrmConsInd.java
+++ POOGPOAU4EQUIPO2/src/SRC/FrmConsInd.java
@@ -102,9 +102,9 @@ public class FrmConsInd extends javax.swing.JDialog {
         }catch(NumberFormatException nfe){
             JOptionPane.showMessageDialog(this, "Ingresa solamente numeros enteros ", 
                             "CARÁCTER INVALIDO", JOptionPane.ERROR_MESSAGE);  
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(this, "Situacion anomala", 
-                            "INCERTIDUMBRE", JOptionPane.ERROR_MESSAGE);
+        }catch(RuntimeException e){
+            JOptionPane.showMessageDialog(this, "Situacion anomala en tiempo de ejecución, vuelve a intentarlo", 
+                            "SITUACIÓN ANOMALA", JOptionPane.ERROR_MESSAGE);
         } 
     }//GEN-LAST:event_btnBuscarActionPerformed
 
