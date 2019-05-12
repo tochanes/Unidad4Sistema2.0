fb689c83f37c7ce597a055292b1fdb0f63c7570e
JanPol2000 <46852049+JanPol2000@users.noreply.github.com>
2019-05-12 00:20:50 -0500

    requestFocus

--- POOGPOAU4EQUIPO2/src/SRC/FrmConsInd.java
+++ POOGPOAU4EQUIPO2/src/SRC/FrmConsInd.java
@@ -26,6 +26,9 @@ public class FrmConsInd extends javax.swing.JDialog {
             public void windowClosed(java.awt.event.WindowEvent evt) {
                 formWindowClosed(evt);
             }
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
         });
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -109,6 +112,10 @@ public class FrmConsInd extends javax.swing.JDialog {
     private void btnVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVolverActionPerformed
         this.dispose();
     }//GEN-LAST:event_btnVolverActionPerformed
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        txtClave.requestFocus();
+    }//GEN-LAST:event_formWindowOpened
 
     private void formWindowClosed(java.awt.event.WindowEvent evt) {                                  
        this.dispose();
